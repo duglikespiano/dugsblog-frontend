597a1ed80e75da9adcb17bcd1925a310b4448d4f
SUHO PARK <duglikespiano@gmail.com>
2025-02-17 22:48:37 +0900

    REFACTOR - COMMIT HISTORY REFACTORED

--- src/components/CommitHistory.tsx
+++ src/components/CommitHistory.tsx
@@ -1,9 +1,20 @@
+import React from 'react';
+import { Tooltip } from 'react-tooltip';
 import GitHubCalendar from 'react-github-calendar';
 
 export default function CommitHistory() {
 	return (
-		<div className="flex justify-center">
-			<GitHubCalendar username="duglikespiano" colorScheme="dark" />
-		</div>
+		<>
+			<GitHubCalendar
+				username="duglikespiano"
+				renderBlock={(block, activity) =>
+					React.cloneElement(block, {
+						'data-tooltip-id': 'react-tooltip',
+						'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+					})
+				}
+			/>
+			<Tooltip id="react-tooltip" />
+		</>
 	);
 }
