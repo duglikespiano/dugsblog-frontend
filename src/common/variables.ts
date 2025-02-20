7f2acb746d35de193f86c94e74780b0c5b0502af
SUHO PARK <duglikespiano@gmail.com>
2025-02-20 15:06:41 +0900

    REFACTOR - ROUTES DATA ADDED

---
+++ src/common/variables.ts
@@ -0,0 +1,22 @@
+export const routes = [
+	{
+		name: 'About',
+		url: 'about',
+	},
+	{
+		name: 'Projects',
+		url: 'projects',
+	},
+	{
+		name: 'Blog',
+		url: 'blog',
+	},
+	{
+		name: 'Contact',
+		url: 'contact',
+	},
+	{
+		name: 'Guestbook',
+		url: 'guestbook',
+	},
+];
