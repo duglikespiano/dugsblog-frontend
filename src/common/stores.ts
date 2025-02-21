c5795cb04fe0cd3a3cfd0d1026235d31063b4344
SUHO PARK <duglikespiano@gmail.com>
2025-02-22 03:11:26 +0900

    REFACTOR - PROJECT PAGE REFACTORED

---
+++ src/common/stores.ts
@@ -0,0 +1,4 @@
+import { atom } from 'nanostores';
+
+export const skillsOriginalStore = atom();
+export const skillsSelectedStore = atom();
--- src/components/projects/SkillsFilter.tsx
+++ src/components/projects/SkillsFilter.tsx
@@ -1,23 +1,21 @@
-import { useState } from 'react';
+import { skillsOriginalStore, skillsSelectedStore } from '../../common/stores';
 
 export default function SkillsFilter({ skillsArray }: { skillsArray: string[] }) {
-	const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
-
-	const handleSkillClick = (skill: string) => {
-		setSelectedSkills((prev) => (prev.includes(skill) ? prev.filter((s) => s !== skill) : [...prev, skill]));
-		console.log('Selected:', skill);
+	const handleSetSkills = (index: number) => {
+		skillsSelectedStore.set(index);
 	};
 
 	return (
-		<div>
-			{skillsArray.map((skill) => (
-				<p key={skill} onClick={() => handleSkillClick(skill)}>
+		<section className="border-6 rounded-4xl flex p-5 flex-wrap gap-3">
+			{skillsArray.map((skill, index) => (
+				<p
+					className={`flex justify-center items-center border-3 rounded-2xl p-2 text-2xl cursor-pointer`}
+					onClick={() => handleSetSkills(index)}
+					key={Math.random()}
+				>
 					{skill}
 				</p>
 			))}
-			<div>
-				<strong>Selected Skills:</strong> {selectedSkills.join(', ')}
-			</div>
-		</div>
+		</section>
 	);
 }
