98ed791c6125f52b36533f46edf18d85ca8c5510
SUHO PARK <duglikespiano@gmail.com>
2025-02-25 14:45:44 +0900

    REFACTOR - TYPE FOR OBJECT ARRAY REFACTORED

--- src/common/stores.ts
+++ src/common/stores.ts
@@ -1,6 +1,5 @@
 import { atom } from 'nanostores';
 import { allOfSkillsIhaveTried, allOfMyProjects } from '../common/variables';
 
-// Initial value is all skills
 export const skillsStore = atom(allOfSkillsIhaveTried);
 export const projectsStore = atom(allOfMyProjects);
