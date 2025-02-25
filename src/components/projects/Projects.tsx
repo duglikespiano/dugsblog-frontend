69d1139c614bfa647f7514a8dc8b783e08f8dabc
SUHO PARK <duglikespiano@gmail.com>
2025-02-25 22:06:20 +0900

    REFACTOR - PROJECTS PAGE REFACTORED

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -7,60 +7,61 @@ export default function Projects() {
 	const selectedSkillsArray = selectedSkills.filter((skill) => skill.isSelected).map((skill) => skill.name);
 
 	return (
-		<section id="projects">
-			<ul className="pt-15 flex flex-col gap-10 px-20">
+		<section id="projects" className="px-20 pt-15">
+			<h3 className="text-rosewater font-bold text-5xl">Projects</h3>
+			<ul className="flex flex-col gap-10 mt-5">
 				{filteredProjects.map((project) => (
 					<li className="border-4 rounded-4xl p-8" key={project.id}>
-						<div className="flex flex-col mb-3">
-							<p className="text-5xl font-bold mb-0.5 text-gray1">{project.title}</p>
-							<p className="text-3xl text-gray-400">{project.descriptionEn}</p>
-						</div>
-						<div className="flex flex-col mb-3 text-gray-500">
-							<div className="flex text-lg items-center">
-								<span>Deployment URL :&nbsp;</span>
-								{project.deploymentURL ? (
-									<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.deploymentURL}>
-										Click here
-									</a>
-								) : (
-									'-'
-								)}
+						<p className="text-4xl font-bold text-gray1 mb-3">{project.title}</p>
+						<div className="px-6">
+							<p className="text-3xl text-gray-400 mb-3">{project.descriptionEn}</p>
+							<div className="flex flex-col mb-3 text-gray-500">
+								<div className="flex text-lg items-center">
+									<span>Deployment URL :&nbsp;</span>
+									{project.deploymentURL ? (
+										<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.deploymentURL}>
+											Click here
+										</a>
+									) : (
+										'-'
+									)}
+								</div>
+								<div className="flex text-lg items-center">
+									<span>Frontend Repository :&nbsp;</span>
+									{project.frontendRepositoryURL ? (
+										<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.frontendRepositoryURL}>
+											Click here
+										</a>
+									) : (
+										'-'
+									)}
+								</div>
+								<div className="flex text-lg items-center">
+									<span>Backend Repository :&nbsp;</span>
+									{project.backendRepositoryURL ? (
+										<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.backendRepositoryURL}>
+											Click here
+										</a>
+									) : (
+										'-'
+									)}
+								</div>
 							</div>
-							<div className="flex text-lg items-center">
-								<span>Frontend Repository :&nbsp;</span>
-								{project.frontendRepositoryURL ? (
-									<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.frontendRepositoryURL}>
-										Click here
-									</a>
-								) : (
-									'-'
-								)}
+							<div>
+								<p className="text-2xl mb-1 text-gray-300 font-bold">Relative Skills</p>
+								<ul className="flex gap-2 flex-wrap text-xl">
+									{project.skills.map((skill: string, index) => (
+										<li
+											className={`border-3 rounded-xl font-bold p-1 ${
+												selectedSkillsArray.includes(skill) ? 'bg-purple4' : 'bg-green2'
+											}`}
+											key={index}
+										>
+											{skill}
+										</li>
+									))}
+								</ul>
 							</div>
-							<div className="flex text-lg items-center">
-								<span>Backend Repository :&nbsp;</span>
-								{project.backendRepositoryURL ? (
-									<a className="border-1 rounded-sm text-xs p-0.5 bg-green2 text-gray-300" href={project.backendRepositoryURL}>
-										Click here
-									</a>
-								) : (
-									'-'
-								)}
-							</div>
-						</div>
-						<div>
-							<p className="text-xl mb-1 text-gray-300 font-bold">Relative Skills</p>
-							<ul className="flex gap-2 flex-wrap text-xl">
-								{project.skills.map((skill: string, index) => (
-									<li
-										className={`border-3 rounded-xl font-bold p-1 ${
-											selectedSkillsArray.includes(skill) ? 'bg-purple4' : 'bg-green2'
-										}`}
-										key={index}
-									>
-										{skill}
-									</li>
-								))}
-							</ul>
 						</div>
 					</li>
 				))}
--- src/components/projects/SkillsFilter.tsx
+++ src/components/projects/SkillsFilter.tsx
@@ -40,7 +40,15 @@ export default function SkillsFilter() {
 			id="skillsFilter"
 			className="relative px-20 pb-15 after:content-[''] after:w-[calc(100%-10rem)] after:h-0.5 after:bg-gray3 after:block after:absolute after:bottom-0 after:left-[50%] after:translate-x-[-50%]"
 		>
-			<ul className="border-4 rounded-4xl flex p-5 flex-wrap gap-2.5">
+			<h3 className="text-rosewater font-bold text-5xl mb-5">Skills filter</h3>
+			<div className="px-6">
+				<p className="text-gray-300 font-bold text-2xl">Click skills to filter projects</p>
+				<p className="text-gray-300 font-bold text-xl flex items-center">
+					<span className="text-yellow1">{projectsStore.get().length}</span>&nbsp;of&nbsp;
+					<span className="text-yellow1">{allOfMyProjects.length}</span>&nbsp;Project(s) selected
+				</p>
+			</div>
+			<ul className="border-4 rounded-4xl flex p-5 flex-wrap gap-2.5 mt-5">
 				{selectedSkills.map((skill, index: number) => (
 					<li
 						className={`flex justify-center items-center border-4 font-bold rounded-2xl p-1 text-2xl cursor-pointer select-none ${
