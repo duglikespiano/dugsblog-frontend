0814a28ca0ad24ad9baa10e46cf213f3619e6840
SUHO PARK <duglikespiano@gmail.com>
2025-03-05 02:08:18 +0900

    DEPENDENCY - OVERLAYSCROLL REACT ADDED

--- src/components/main/CommitHistory.tsx
+++ src/components/main/CommitHistory.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Tooltip } from 'react-tooltip';
 import GitHubCalendar from 'react-github-calendar';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
+import './CommitHistory.css';
 
 export default function CommitHistory() {
 	return (
 		<section id="commit-history" className="flex justify-center py-16">
-			<GitHubCalendar
-				username="duglikespiano"
-				renderBlock={(block, activity) =>
-					React.cloneElement(block, {
-						'data-tooltip-id': 'react-tooltip',
-						'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
-					})
-				}
-				labels={{
-					totalCount: `{{count}} activities for the past year`,
-				}}
-			/>
+			<OverlayScrollbarsComponent defer>
+				<GitHubCalendar
+					username="duglikespiano"
+					renderBlock={(block, activity) =>
+						React.cloneElement(block, {
+							'data-tooltip-id': 'react-tooltip',
+							'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
+						})
+					}
+					labels={{
+						totalCount: `{{count}} activities for the past year`,
+					}}
+				/>
+			</OverlayScrollbarsComponent>
 			<Tooltip id="react-tooltip" />
 		</section>
 	);
