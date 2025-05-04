47174323df566f9011326433418bedb1eb5dde20
SUHO PARK <duglikespiano@gmail.com>
2025-05-04 11:43:52 +0900

    REFACTOR - SKILLS FILTER COMPONENT REFACTORED
    REFACTOR - SKILLS FILTER COMPONENT REFACTORED

--- src/components/projects/SkillsFilter.tsx
+++ src/components/projects/SkillsFilter.tsx
@@ -1,66 +1,83 @@
-import { useStore } from '@nanostores/react';
-import { skillsStore, projectsStore } from '../../common/stores';
-import { allOfSkillsIhaveTried, allOfMyProjects } from '../../common/variables';
-import type { ProjectObjectType } from '../../common/variables';
+import { useStore } from "@nanostores/react";
+import { skillsStore, projectsStore } from "../../common/stores";
+import { allOfSkillsIhaveTried, allOfMyProjects } from "../../common/variables";
+import type { ProjectObjectType } from "../../common/variables";
 
 export default function SkillsFilter() {
-	const selectedSkills = useStore(skillsStore);
+  const selectedSkills = useStore(skillsStore);
 
-	const toggleSkillsSelect = (index: number) => {
-		selectedSkills[index].isSelected = !selectedSkills[index].isSelected;
-		skillsStore.set([...selectedSkills]);
-	};
+  const toggleSkillsSelect = (index: number) => {
+    selectedSkills[index].isSelected = !selectedSkills[index].isSelected;
+    skillsStore.set([...selectedSkills]);
+  };
 
-	const handleDisplayProjects = (event: React.MouseEvent<HTMLLIElement>, index: number) => {
-		toggleSkillsSelect(index);
+  const handleDisplayProjects = (
+    event: React.MouseEvent<HTMLLIElement>,
+    index: number,
+  ) => {
+    toggleSkillsSelect(index);
 
-		const selectedSkillsArray = selectedSkills.filter((skill) => skill.isSelected).map((skill) => skill.name);
-		const filteredProjects: ProjectObjectType[] = [];
+    const selectedSkillsArray = selectedSkills
+      .filter((skill) => skill.isSelected)
+      .map((skill) => skill.name);
+    const filteredProjects: ProjectObjectType[] = [];
 
-		selectedSkillsArray.forEach((selectedSkill) => {
-			allOfMyProjects.forEach((project) => {
-				project.skills.forEach((skill) => {
-					if (selectedSkill === skill) {
-						filteredProjects.push(project);
-					}
-				});
-			});
-		});
+    selectedSkillsArray.forEach((selectedSkill) => {
+      allOfMyProjects.forEach((project) => {
+        project.skills.forEach((skill) => {
+          if (selectedSkill === skill) {
+            filteredProjects.push(project);
+          }
+        });
+      });
+    });
 
-		if (allOfSkillsIhaveTried.length === selectedSkills.filter((skill) => !skill.isSelected).length) {
-			projectsStore.set(allOfMyProjects);
-		} else {
-			const sortedProjects = filteredProjects.sort((a, b) => new Date(b.period[0]).getTime() - new Date(a.period[0]).getTime());
-			projectsStore.set(Array.from(new Set(sortedProjects)) as ProjectObjectType[]);
-		}
-	};
+    if (
+      allOfSkillsIhaveTried.length ===
+      selectedSkills.filter((skill) => !skill.isSelected).length
+    ) {
+      projectsStore.set(allOfMyProjects);
+    } else {
+      const sortedProjects = filteredProjects.sort(
+        (a, b) =>
+          new Date(b.period[0]).getTime() - new Date(a.period[0]).getTime(),
+      );
+      projectsStore.set(
+        Array.from(new Set(sortedProjects)) as ProjectObjectType[],
+      );
+    }
+  };
 
-	return (
-		<section
-			id="skillsFilter"
-			className="relative px-20 pb-15 after:content-[''] after:w-[calc(100%-10rem)] after:h-0.5 after:bg-gray3 after:block after:absolute after:bottom-0 after:left-[50%] after:translate-x-[-50%]"
-		>
-			<h3 className="text-rosewater font-bold text-5xl mb-5">Skills filter</h3>
-			<div className="px-6">
-				<p className="text-gray-400 font-bold text-2xl">Click skills to filter projects</p>
-				<p className="text-gray-500 font-bold text-xl flex items-center">
-					<span className="text-yellow1">{projectsStore.get().length}</span>&nbsp;of&nbsp;
-					<span className="text-yellow1">{allOfMyProjects.length}</span>&nbsp;Project(s) selected
-				</p>
-			</div>
-			<ul className="border-4 rounded-4xl flex p-5 flex-wrap gap-2.5 mt-5">
-				{selectedSkills.map((skill, index: number) => (
-					<li
-						className={`flex justify-center items-center border-4 font-bold rounded-2xl p-1 text-2xl cursor-pointer select-none ${
-							skill.isSelected ? 'bg-purple4' : 'bg-green2'
-						}`}
-						onClick={(event) => handleDisplayProjects(event, index)}
-						key={skill.id}
-					>
-						{skill.name}
-					</li>
-				))}
-			</ul>
-		</section>
-	);
+  return (
+    <section
+      id="skillsFilter"
+      className="after:bg-gray3 relative px-5 pb-15 after:absolute after:bottom-0 after:left-[50%] after:block after:h-0.5 after:w-[calc(100%-(var(--spacing)*5)*2)] after:translate-x-[-50%] after:content-[''] lg:px-20 lg:after:w-[calc(100%-10rem)]"
+    >
+      <h3 className="text-rosewater mb-5 text-5xl font-bold">Skills filter</h3>
+      <div className="px-6">
+        <p className="text-2xl font-bold text-gray-400">
+          Click skills to filter projects
+        </p>
+        <p className="flex items-center text-xl font-bold text-gray-500">
+          <span className="text-yellow1">{projectsStore.get().length}</span>
+          &nbsp;of&nbsp;
+          <span className="text-yellow1">{allOfMyProjects.length}</span>
+          &nbsp;Project(s) selected
+        </p>
+      </div>
+      <ul className="mx-6 mt-5 flex flex-wrap gap-2.5 rounded-4xl border-4 p-5">
+        {selectedSkills.map((skill, index: number) => (
+          <li
+            className={`flex cursor-pointer items-center justify-center rounded-2xl border-4 p-1 text-2xl font-bold select-none ${
+              skill.isSelected ? "bg-purple4" : "bg-green2"
+            }`}
+            onClick={(event) => handleDisplayProjects(event, index)}
+            key={skill.id}
+          >
+            {skill.name}
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
 }
