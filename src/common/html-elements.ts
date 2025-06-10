bc942f919603fbf9c27047fbf1c317fd519fb462
SUHO PARK <duglikespiano@gmail.com>
2025-06-11 01:06:48 +0900

    REFACTOR - NAV COMPONENET FOR KOREAN PAGE REFACTORED

---
+++ src/common/html-elements.ts
@@ -0,0 +1,39 @@
+export const logoInfo = {
+  en: "Dug's Blog",
+  ko: "더그의 블로그",
+  ja: "ダグのブログ",
+  href: "",
+};
+
+export const navInfo = {
+  about: {
+    en: "About",
+    ko: "소개",
+    ja: "紹介",
+    href: "/about",
+  },
+  projects: {
+    en: "projects",
+    ko: "프로젝트",
+    ja: "プロジェクト",
+    href: "/projects",
+  },
+  blog: {
+    en: "blog",
+    ko: "블로그",
+    ja: "ブログ",
+    href: "/blog",
+  },
+  contact: {
+    en: "contact",
+    ko: "문의",
+    ja: "お問い合わせ",
+    href: "/contact",
+  },
+  guestbook: {
+    en: "guestbook",
+    ko: "방명록",
+    ja: "ゲストブック",
+    href: "/guestbook",
+  },
+};
