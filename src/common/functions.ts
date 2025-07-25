1defc75de2ff3ff91a82f804d6d6eaca955db10b
SUHO PARK <duglikespiano@gmail.com>
2025-07-26 02:20:44 +0900

    FEATURE - HTML DOCUMENT LANGUAGE SPECIFYING FUNCTION ADDED

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -7,3 +7,13 @@ export function printFileURL(url: string) {
   const folderName = fullFilePath.split("[language]/")[1];
   return folderName;
 }
+
+export function printHTMLDocumentLanguage(language: string) {
+  if (language === "en") {
+    return "en-US";
+  } else if (language === "ko") {
+    return "ko-KR";
+  } else if (language === "ja") {
+    return "ja-JP";
+  }
+}
