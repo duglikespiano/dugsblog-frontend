60e5f66b0281b9f30694d0eeb92f5f070a2fdc2d
SUHO PARK <duglikespiano@gmail.com>
2025-07-31 19:45:57 +0900

    FEATURE - TYPE ASSERTION FOR POST AND TAG ADDED

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,5 @@
+import { z } from "astro:schema";
+
 export type Languages = "en" | "ko" | "ja";
 export type Pages = "" | "about" | "projects" | "contact" | "guestbook";
 export type Project = {
@@ -19,4 +21,22 @@ export const collectionMap = {
   ko: "koPosts",
   ja: "jaPosts",
 } as const;
-export type Tags = "programming";
+export const tags = [
+  "programming",
+  "javascript",
+  "css",
+  "html",
+  "php",
+  "framework",
+  "travel",
+  "career",
+  "thoughts",
+] as const;
+export type Tags = (typeof tags)[number];
+export const postSchema = z.object({
+  title: z.string(),
+  slug: z.string(),
+  tags: z.array(z.enum(tags)),
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+});
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,37 +1,18 @@
 import { defineCollection, z } from "astro:content";
+import { postSchema } from "./common/types";
 import { glob } from "astro/loaders";
 
 const enPosts = defineCollection({
   loader: glob({ pattern: "*.mdx", base: "./src/content/blog/en/" }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    tags: z.string().array(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
+  schema: postSchema,
 });
-
 const koPosts = defineCollection({
   loader: glob({ pattern: "*.mdx", base: "./src/content/blog/ko/" }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    tags: z.string().array(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
+  schema: postSchema,
 });
-
 const jaPosts = defineCollection({
   loader: glob({ pattern: "*.mdx", base: "./src/content/blog/ja/" }),
-  schema: z.object({
-    title: z.string(),
-    slug: z.string(),
-    tags: z.string().array(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-  }),
+  schema: postSchema,
 });
 
 export const collections = { enPosts, koPosts, jaPosts };
