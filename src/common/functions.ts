dc0986ae57523bbc23ccada53f142ef6a11e0151
SUHO PARK <duglikespiano@gmail.com>
2025-08-01 20:30:26 +0900

    REFACTOR - ERROR PAGE RELATED LOGIC REFACTORED

--- src/common/functions.ts
+++ src/common/functions.ts
@@ -63,3 +63,7 @@ export function capitalizeFirstLetter(str: string) {
   }
   return `${str.charAt(0).toUpperCase()}${str.slice(1)}`;
 }
+
+export function isStringLanguage(value: string): boolean {
+  return ["en", "ko", "ja"].includes(value);
+}
