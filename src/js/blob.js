e89bbc33f9996bc6971372c51ed468ca353e870f
SUHO PARK <duglikespiano@gmail.com>
2025-08-03 04:27:50 +0900

    REFACTOR - ANIMATION RELATED CODE REFACTORED

--- src/js/blob.js
+++
@@ -1,6 +0,0 @@
-const rootWrapper = document.querySelector("[data-root-wrapper]");
-const blob = document.createElement("span");
-blob.classList.add("blob");
-rootWrapper.appendChild(blob);
-
-console.log(rootWrapper);
