dba61962434ce4f48f2a01268764f60384351ebc
SUHO PARK <duglikespiano@gmail.com>
2025-08-05 00:56:33 +0900

    FEATURE - CACHE BUSTING MIDDLEWARE ADDED

---
+++ src/middleware.ts
@@ -0,0 +1,7 @@
+import type { MiddlewareHandler } from "astro";
+
+export const onRequest: MiddlewareHandler = async (context, next) => {
+  const response = await next();
+  response.headers.set("Cache-Control", "no-store");
+  return response;
+};
