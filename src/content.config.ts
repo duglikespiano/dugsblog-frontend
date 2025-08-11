06b523e3234b0b731feb4ef3de3a180ea46783db
SUHO PARK <duglikespiano@gmail.com>
2025-08-12 01:59:45 +0900

    REFACTOR - FILES DIRECTORY REFACTORED

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,15 @@ import { postSchema } from "./common/types";
 import { glob } from "astro/loaders";
 
 const enPosts = defineCollection({
-  loader: glob({ pattern: "**/en.mdx", base: "./src/content/" }),
+  loader: glob({ pattern: "**/en.mdx", base: "./src/contents/" }),
   schema: postSchema,
 });
 const koPosts = defineCollection({
-  loader: glob({ pattern: "**/ko.mdx", base: "./src/content/" }),
+  loader: glob({ pattern: "**/ko.mdx", base: "./src/contents/" }),
   schema: postSchema,
 });
 const jaPosts = defineCollection({
-  loader: glob({ pattern: "**/ja.mdx", base: "./src/content/" }),
+  loader: glob({ pattern: "**/ja.mdx", base: "./src/contents/" }),
   schema: postSchema,
 });
 
