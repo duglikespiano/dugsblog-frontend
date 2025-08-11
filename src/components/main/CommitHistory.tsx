0671a3d3860c97613c3ef8284bd74fbe2ab865b9
SUHO PARK <duglikespiano@gmail.com>
2025-08-12 01:54:20 +0900

    REFACTOR - COMMIT HISTORY MISC STYLE REFACTORED

--- src/components/main/CommitHistory.tsx
+++ src/components/main/CommitHistory.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { Tooltip } from 'react-tooltip';
-import GitHubCalendar from 'react-github-calendar';
-import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
-import './CommitHistory.css';
+import React from "react";
+import { Tooltip } from "react-tooltip";
+import GitHubCalendar from "react-github-calendar";
+import { OverlayScrollbarsComponent } from "overlayscrollbars-react";
+import "./CommitHistory.css";
 
 export default function CommitHistory() {
-	return (
-		<section id="commit-history" className="flex justify-center py-16">
-			<OverlayScrollbarsComponent defer>
-				<GitHubCalendar
-					username="duglikespiano"
-					renderBlock={(block, activity) =>
-						React.cloneElement(block, {
-							'data-tooltip-id': 'react-tooltip',
-							'data-tooltip-html': `${activity.count} activities on ${activity.date}`,
-						})
-					}
-					labels={{
-						totalCount: `{{count}} activities for the past year`,
-					}}
-				/>
-			</OverlayScrollbarsComponent>
-			<Tooltip id="react-tooltip" />
-		</section>
-	);
+  return (
+    <section id="commit-history" className="flex justify-center pt-15 pb-15">
+      <OverlayScrollbarsComponent defer>
+        <GitHubCalendar
+          username="duglikespiano"
+          renderBlock={(block, activity) =>
+            React.cloneElement(block, {
+              "data-tooltip-id": "react-tooltip",
+              "data-tooltip-html": `${activity.count} activities on ${activity.date}`,
+            })
+          }
+          labels={{
+            totalCount: `{{count}} activities for the past year`,
+          }}
+        />
+      </OverlayScrollbarsComponent>
+      <Tooltip id="react-tooltip" />
+    </section>
+  );
 }
