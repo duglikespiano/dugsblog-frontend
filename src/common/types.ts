745d644ed5a03ba41717c4960dd5d3be0ec30eb4
SUHO PARK <duglikespiano@gmail.com>
2026-01-18 00:22:31 +0900

    POST - A RECORD CNAME ALIAS

--- src/common/types.ts
+++ src/common/types.ts
@@ -25,7 +25,7 @@ export const collectionMap = {
   ja: "jaPosts",
 } as const;
 
-export const tagKeys = ["travel", "thoughts", "programming", "javascript", "english"] as const;
+export const tagKeys = ["travel", "thoughts", "programming", "javascript", "english", "network"] as const;
 // export const tagKeys = ["programming", "javascript", "css", "html", "php", "framework"] as const;
 export type TagLang = (typeof tagKeys)[number];
 
@@ -36,6 +36,7 @@ export const tags: Record<TagLang, TagTranslations> = {
   // html: { en: "HTML", ko: "HTML", ja: "HTML" },
   // php: { en: "PHP", ko: "PHP", ja: "PHP" },
   // framework: { en: "Framework", ko: "프레임워크", ja: "フレームワーク" },
+  network: { en: "Network", ko: "네트워크", ja: "ネットワーク" },
   travel: { en: "Travel", ko: "여행", ja: "旅行" },
   thoughts: { en: "Thoughts", ko: "생각", ja: "考え" },
   english: { en: "English", ko: "영어", ja: "英語" },
